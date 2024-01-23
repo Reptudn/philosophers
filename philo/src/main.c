5b70af401fb3397bbc2753ff7230dd1936181f7b
Jonas Kauker <jkauker@2-H-3.42heilbronn.de>
2024-01-23 09:33:15 +0100

    a

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,23 +6,27 @@
 /*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 09:46:02 by jkauker           #+#    #+#             */
-/*   Updated: 2024/01/22 10:32:40 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/01/23 09:33:07 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+#include <pthread.h>
 
 int	spawn_philos(t_program *program)
 {
 	pid_t	pid;
 
 	program->current_philos = -1;
+	program->philos = malloc(sizeof(t_philo) * program->number_of_philosophers);
+	if (!program->philos)
+		return (0);
 	while (++program->current_philos < program->number_of_philosophers)
 	{
-		// spawn philo
+		program->philos[program->current_philos].id = program->current_philos;
 		printf("[\t%d\t] Philosopher spawned\n", program->current_philos);
 	}
-	return (0);
+	return (1);
 }
 
 int	main(int argc, char **argv)
