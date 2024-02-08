7e3375c70df68c161d355e04465d970d3aafb406
Jonas Kauker <jkauker@3-B-14.42heilbronn.de>
2024-02-08 14:39:46 +0100

    fixed gitignore

---
+++ philo/utils/time.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 14:52:55 by jkauker           #+#    #+#             */
+/*   Updated: 2024/02/06 14:59:04 by jkauker          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+// returns current time in microseconds
+int	get_current_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000000 + time.tv_usec);
+}
+
+int	ft_usleep(int time_in_ms, int last_eat, t_program *program)
+{
+	int	start;
+
+	start = get_current_time();
+	while (get_current_time() - start < time_in_ms)
+	{
+		if (program->dead == 1)
+			return (0);
+		if (get_current_time() - last_eat >= program->time_to_die)
+		{
+			if (program->dead == 0)
+				program->dead = 1;
+			return (0);
+		}
+		usleep(250);
+	}
+	return (1);
+}
+
+float	get_converted_time(int start)
+{
+	return ((float)(get_current_time() - start) / 1000);
+}
